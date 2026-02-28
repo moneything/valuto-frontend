a6e7381f0c49932eb8794dd4334cef32d57b0d7c
Nick Efe Oni <neoni1@sheffield.ac.uk>
2026-02-28 02:18:56 +0000

    feat(contact): implement initial contact page layout
    created a new contact page that includes a header and footer component. the page currently displays a message indicating that it is still in progress. this change lays the groundwork for future enhancements and provides a basic structure for user interaction. there are no immediate risks, but further development will be needed to complete the functionality.

---
+++ frontend/app/contact/page.tsx
@@ -0,0 +1,15 @@
+"use client";
+
+import Footer from "@/components/Footer";
+import Header from "@/components/Header";
+
+export default function Features() {
+  return (
+    <>
+        <Header />
+        <p className="text-white py-32">Contact Page In Progress</p>
+        <Footer />
+    </>
+  );
+}
+
